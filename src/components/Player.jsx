01a018adf973f4cc5e5bfa5232c8be0be1f288c5
Ian Shaw <ianshaw@firedynamicsgroup.com>
2023-04-07 15:41:42 +0100

    removed basket in readiness for hud showing newspapers left to throw, some refactoring

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -118,40 +118,18 @@ export default function Player({canvasIsClicked, onPaperLocationChange}) {
 
         state.camera.position.copy(smoothedCameraPositon)
         state.camera.lookAt(smoothedCameraTarget)
-
-        // basket trial at alternative to moving newspaper location - have it sit in a basket that moves with player
-        basketRef.current.position.copy(playerPosition)
-        basketRef.current.position.y -= 0.10
-        basketRef.current.position.z += 0.30 
-        
-        // move unused papers in relation to basket
-        unusedPapersGroupRef.current.position.copy(basketRef.current.position)
-
-        // if(throwingNewspaper.current) {
-        //     if (throwingNewspaper.current.linvel().x == 0) {
-
-        //         console.log("still throwingNewspaper location: ", throwingNewspaper.current.translation())
-                
-        //     } 
-        // }
-        
         
         if (aiming && canvasIsClicked) { // aiming
             /**
-             * record location of newspaper if throwing == true
-             * later change mesh to other rigidbody with same angular velocity
-             * then change throwing to false
-             * below -> throwing paper from state
+             * moves paper before throw is actioned on mouse up
             */
            
            if(!thrown && throwingNewspaper.current){
-               // change z to behind object
-                // need to convert pointer position to 3d space
                 // attempt -> paper always above body and transaparent paper with hand??
                 // todo: have zero spinning; stay flat until thrown?
                 throwingNewspaper.current.setTranslation({x: playerPosition.x, y: playerPosition.y+0.6, z: playerPosition.z + 0.2})
-                
-                // throwingNewspaper.current.setTranslation({x: state.pointer.x, y: state.pointer.y+0.3, z: playerPosition.z + 0.4})
+                console.log("currentThrowingPaper: ", currentThrowingPaper)
+                console.log("paperQuantity: ", paperQuantity)
             }
             
 
@@ -165,7 +143,7 @@ export default function Player({canvasIsClicked, onPaperLocationChange}) {
 
         }
         // throwing when pointer lifted
-        if (aiming && !canvasIsClicked) {
+        if (aiming && !canvasIsClicked && throwingNewspaper.current) {
             // use pointer location @ release -> -1 to 1
             // TODO: use magnitude of each x and y
             // setThrowing(true)
@@ -180,7 +158,6 @@ export default function Player({canvasIsClicked, onPaperLocationChange}) {
             console.timeEnd("pointerUp")
 
             throwingNewspaper.current.applyImpulse(impulse)
-            // does below re-render mean reset of moving positions? - save current positions?
             setThrown(true)
             setAiming(false)
             // below should be actioned on aiming but returned to pile if not thrown
@@ -195,27 +172,23 @@ export default function Player({canvasIsClicked, onPaperLocationChange}) {
     })
 
     function initAim(event) {
-        if (thrown) {
-            // setCurrentThrowingPaper((current) => (current < numberOfPapers - 2 ) ? current + 1 : current)
-            // if (currentThrowingPaper < numberOfPapers - 2) {
-
-            //     throwingNewspaper = paperRefs.current[currentThrowingPaper + 1]
-            // }
-            // check size of location array
-            // should push to array
-            // too slow?
+        if (thrown) { // not actioned on first go; only after at least 1 has been thrown
             console.log("update paper location: ", throwingNewspaper.current.translation())
-            onPaperLocationChange(throwingNewspaper.current.translation())
+            // later auto locate when newspaper comes to rest
+            onPaperLocationChange(throwingNewspaper.current.translation()) // how to send location of last paper?
             // check that linvel is 0
             setThrown(false)
         }
         console.log("event: ",event)
-        // only if newspapers are left to throw!
-        // Done: have object in place of newspaper - > cube for now
-        // TODO: allow for drag of mouse -> distance of drag more force
-        setPointLocation(event.point)
-        setAiming(true)
-        // on release -> throw newspaper
+        if (paperQuantity >= 0) {
+
+            // only if newspapers are left to throw!
+            // Done: have object in place of newspaper - > cube for now
+            // TODO: allow for drag of mouse -> distance of drag more force
+            setPointLocation(event.point)
+            setAiming(true)
+            // on release -> throw newspaper
+        }
     }
 
     const newspaperShell = (
@@ -225,26 +198,6 @@ export default function Player({canvasIsClicked, onPaperLocationChange}) {
     </>
     )
 
-    
-    // if (playerRef.current && throwingNewspaper.current) {
-    //     // not sure if this is working?
-    //     throwingNewspaper.current.interactionGroups = ~ 1
-    //     playerRef.current.interactionGroups = ~2
-        
-    // }
-    // later send paperQuantity in via props - may need state if 1 subtracted each time removed from pile?
-    // TODO: use current throwing paper state -> relates to ref
-    // 
-    const unusedPapers = []
-    for (let i = 0; i < paperQuantity; i++){
-        (
-            unusedPapers.push( 
-                <mesh castShadow position={[i/5 - 0.5, 0, 0.01]} key={i}>
-                    {newspaperShell}
-                </mesh>
-            )        
-            )}
-
     /**
      *     // TODO: sense that paper landed on house tile -> throwing paper before moved again
     // perhaps change thrown with normal paper mesh
@@ -252,10 +205,7 @@ export default function Player({canvasIsClicked, onPaperLocationChange}) {
 
 
     return <>
-    {/* 
-        TODO: player body should not collide with newspaper being thrown 
 
-    */}
     <RigidBody
         ref={ playerRef }
         restitution={ 0.2 }
@@ -277,40 +227,8 @@ export default function Player({canvasIsClicked, onPaperLocationChange}) {
         </mesh>
     </RigidBody>
      {/*
-        create group for newspaper pile in basket
+        TODO: create bag for papers
      */}
-        <group ref={unusedPapersGroupRef}>
-            {unusedPapers}
-        </group>
-    {/* // if throwing == False; newspaper.position = body.position + offset */}
-    {/* temp shelf for papers -> until model made
-    newspaper pile located in relation to basket */}
-    <mesh castShadow ref={basketRef}>
-        <boxGeometry args={ [ 1, 0.001, 1 ] } />
-        <meshStandardMaterial flatShading color="yellow" />
-    </mesh>
-    {/* 
-        future group nonRigidbodies (piles of non thrown newspapers) -> next in line becomes rigidbody
-        perhaps have rigidbody change back to non once at rest after throw
-        // use array of ref => rigidbody if is current throwing paper or has been thrown
-        // otherwise -> not a mesh (i.e. represented by the unusedPapers group -> removed from pile when currently thrown paper)
-    */}
-{/* {(aiming || thrown)?<RigidBody
-    ref={throwingNewspaper}
-    restitution={ 0.2 }
-    friction={ 1 } 
-    linearDamping={ 0.5 }
-    angularDamping={ 0.5 }
-    // change z below to behind player position
-    // later to be previous paper position
-    position={[ pointLocation.x, pointLocation.y, pointLocation.z ]}
-    collisionGroup={2}
-    >
-        <mesh castShadow>
-            {newspaperShell}
-        </mesh>
-    </RigidBody>: null
-    } */}
 
     {Array.from({length: numberOfPapers}, (_, index) => {
         return( ((aiming || thrown) && index <= currentThrowingPaper) ?
@@ -320,8 +238,7 @@ export default function Player({canvasIsClicked, onPaperLocationChange}) {
             friction={ 1 } 
             linearDamping={ 0.5 }
             angularDamping={ 0.5 }
-            // change z below to behind player position
-            // later to be previous paper position
+            // hack -> position off screen, then teleported above box when aiming
             position={[ 0, -5, -1 ]}
             collisionGroup={2}
             key={index}
